4ef74bfe8865e13fc694debb92d4f138bb0f4a17
d <dmitriiprozherin@mail.ru>
2023-11-16 21:03:30 +0100

    Additional problem 1 solved

---
+++ src/main/java/org/algos/_4/lection_2/C.java
@@ -0,0 +1,9 @@
+package org.algos._4.lection_2;
+
+public class C {
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ src/main/java/org/algos/_4/lection_2/Task2.java
@@ -0,0 +1,7 @@
+package org.algos._4.lection_2;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+    }
+}
