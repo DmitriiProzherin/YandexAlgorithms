d950e81e174075499d0e4c5f223e40de789a70ac
d <dmitriiprozherin@gmail.com>
2023-12-21 20:11:20 +0100

    Training 1. Solved: 4.B 4.C

--- src/main/java/org/algos/_1/lection_4/B.java
+++ src/main/java/org/algos/_1/lection_4/B.java
@@ -1,7 +1,53 @@
 package org.algos._1.lection_4;
 
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class B {
     public static void main(String[] args) {
-
+        try (
+                BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/_1/lection_4/B/input.txt"));
+                BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/_1/lection_4/B/output.txt"));
+        ) {
+            Map<String, Integer> map = new HashMap<>();
+            StringBuilder word = new StringBuilder();
+            Integer count;
+            int c;
+            while ((c = reader.read()) > -1)
+            {
+                if (c == ' ' && !word.isEmpty()) {
+                    count = map.get(word.toString());
+                    if (count == null) {
+                        writer.write("0 ");
+                        count = 0;
+                    }
+                    else {
+                        writer.write(count + " ");
+                    }
+                    map.put(word.toString(), ++count);
+                    word.setLength(0);
+                }
+                else if (c == '\n') {
+                    if (!word.isEmpty()){
+                        count = map.get(word.toString());
+                        if (count == null) {
+                            writer.write("0 ");
+                            count = 0;
+                        }
+                        else {
+                            writer.write(count + " ");
+                        }
+                        map.put(word.toString(), ++count);
+                        word.setLength(0);
+                    }
+                }
+                else {
+                    word.append((char) c);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/main/java/org/algos/_1/lection_4/C.java
@@ -0,0 +1,65 @@
+package org.algos._1.lection_4;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class C {
+    public static void main(String[] args) {
+        try (
+                BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/_1/lection_4/C/input.txt"));
+                BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/_1/lection_4/C/output.txt"));
+        ) {
+            Map<String, Integer> map = new HashMap<>();
+            StringBuilder word = new StringBuilder();
+            Integer count;
+            int c;
+            while ((c = reader.read()) > -1)
+            {
+                if (c == ' ' && !word.isEmpty()) {
+                    count = map.get(word.toString());
+                    if (count == null) {
+                        count = 0;
+                    }
+                    map.put(word.toString(), ++count);
+                    word.setLength(0);
+                }
+                else if (c == '\n') {
+                    if (!word.isEmpty()){
+                        count = map.get(word.toString());
+                        if (count == null) {
+                            count = 0;
+                        }
+                        map.put(word.toString(), ++count);
+                        word.setLength(0);
+                    }
+                }
+                else {
+                    word.append((char) c);
+                }
+            }
+
+            if (map.isEmpty()) {
+                writer.write("");
+            }
+            else {
+                int max = 1;
+                Optional<String> opt = map.keySet().stream().findAny();
+                String res = opt.get();
+                for (var e : map.entrySet()) {
+                    if (e.getValue() == max) {
+                        if (e.getKey().compareTo(res) < 0) res = e.getKey();
+                    }
+                    else if (e.getValue() > max) {
+                        max = e.getValue();
+                        res = e.getKey();
+                    }
+                }
+                writer.write(res);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
